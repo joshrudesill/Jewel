0eed5a4b100af80ae0359ab2f28182e2a3222689
josh <joshrudesill@gmail.com>
2022-08-04 18:28:45 +0000

    small changes to appointment cancelling

--- pages/api/cancelapt.js
+++ pages/api/cancelapt.js
@@ -7,13 +7,15 @@ export default async function handler(req, res) {
     const { adminID, aptID } = req.body
     const cook = req.headers.cookie;
     const parsed = cookie.parse(cook)
-    const auth = await verifyJWT(parsed.token, req.body.adminID);
+    const auth = verifyJWT(parsed.token, req.body.adminID);
     if (auth.auth && auth.act === 'admin' && auth.id === adminID) {
-        const apt = await model.Appointments.findOne({where: { id: aptID }})
+        const apt = await model.Appointments.findByPk(aptID)
         if(apt && apt.adminID === adminID) {
             const deleted = await model.Appointments.destroy({ where: { id: aptID }})
             if(deleted > 0) {
                 res.status(200).send()
+            } else {
+                res.status(400).send()
             }
         } else {
             res.status(400).send()
