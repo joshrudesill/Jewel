f43607e8b999ac196fa52f41309afbda88709ea2
Joshua Rudesill <joshrudesill@gmail.com>
2022-09-16 20:26:55 +0200

    started breaks in schedule maker

--- pages/api/createschedule.js
+++ pages/api/createschedule.js
@@ -51,6 +51,11 @@ export default async function handler(req, res) {
         const fm =  from.substring(from.indexOf(':') +1, from.length)
         const th =  to.substring(0, to.indexOf(':'))
         const tm = to.substring(to.indexOf(':') +1, to.length)
+
+        const efh = efrom.substring(0, efrom.indexOf(':'))
+        const efm = efrom.substring(efrom.indexOf(':') +1, efrom.length)
+        const eth =  eto.substring(0, eto.indexOf(':'))
+        const etm = eto.substring(eto.indexOf(':') +1, eto.length)
     
         const fromTime = dayjs().set('hour', parseInt(fh)).set('minute', fm).second(0).millisecond(0)
         const toTime = dayjs().set('hour', th).set('minute',  tm).second(0).millisecond(0)
@@ -69,6 +74,7 @@ export default async function handler(req, res) {
             const time = dayjs(a.startTime)
             const from = dayjs(a.startTime).hour(fh).minute(fm)
             const to = dayjs(a.startTime).hour(th).minute(tm)
+
             if(!daysOfWeek.includes(time.day())) {
                 if(time.isBetween(from, to)) {
                     toDeleteIDs.push(a.id)
@@ -95,17 +101,23 @@ export default async function handler(req, res) {
             }
             if(daysOfWeek.includes(loopStartTime.day())) {
                 for(let i = 0; i < numAptsInPeriod; i++) {
-                   const time = loopStartTime.add(length * i, 'minutes')
-                   var apt = {
-                       userID: null,
-                       adminID: auth.id,
-                       startTime: time.utc(true).toDate(),
-                       endTime: time.utc(true).add(length, 'minutes').toDate(),
-                   }
-                   if(type !== 0) {
-                       apt.aptType = type
-                   }
-                   aptsToAdd.push(apt)
+                    const time = loopStartTime.add(length * i, 'minutes')
+                    const efrom = dayjs(loopStartTime).hour(efh).minute(efm)
+                    const eto = dayjs(loopStartTime).hour(eth).minute(etm)
+                    if(!time.isBetween(efrom, eto) && !time.add(length, 'minutes').isBetween(efrom, eto)) {
+                        var apt = {
+                            userID: null,
+                            adminID: auth.id,
+                            startTime: time.utc(true).toDate(),
+                            endTime: time.utc(true).add(length, 'minutes').toDate(),
+                        }
+                        if(type !== 0) {
+                            apt.aptType = type
+                        }
+                        aptsToAdd.push(apt)
+                    } else {
+                        
+                    }
                }
             }
         }
