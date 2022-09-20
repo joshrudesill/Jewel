59b9b57978ce48cfdf0666c99752f967f291a937
Joshua Rudesill <joshrudesill@gmail.com>
2022-09-20 16:04:41 +0200

    refreshing due to small error

--- components/creator-navbar.js
+++ components/creator-navbar.js
@@ -2,18 +2,18 @@
 
 const CreatorNavBar = () => {
     return (
-        <nav className="navbar p-3">
-            <div className="navbar-brand">
-            <span class="icon-text p-3">
-                <span class="icon">
-                <span className="icon mr-3 has-text-dark"><ion-icon size='large' name="diamond-sharp"></ion-icon></span>
+        <nav classNameName="navbar p-3">
+            <div classNameName="navbar-brand">
+            <span className="icon-text p-3">
+                <span className="icon">
+                <span classNameName="icon mr-3 has-text-dark"><ion-icon size='large' name="diamond-sharp"></ion-icon></span>
                 </span>
-                <span className="is-size-3 has-text-weight-light">Jewel</span>
+                <span classNameName="is-size-3 has-text-weight-light">Jewel</span>
             </span>
             </div>
-            <div className="navbar-menu">
-                <div className="navbar-start">
-                    <a class="navbar-item">
+            <div classNameName="navbar-menu">
+                <div classNameName="navbar-start">
+                    <a className="navbar-item">
                         Home
                     </a>
 
