1ab1235d43b8188371e5526a2b73a55dbff86cf7
Joshua Rudesill <joshrudesill@gmail.com>
2022-09-29 13:27:52 +0200

    small bug

--- pages/api/createtrialaccount.js
+++ pages/api/createtrialaccount.js
@@ -58,7 +58,8 @@ export default async function handler(req, res) {
                     startTime: a.startTime,
                     endTime: a.endTime,
                     message: a.message,
-                    userEmail: a.userEmail
+                    userEmail: a.userEmail,
+                    aptType: a.aptType
                 }
                 aptsToAdd.push(ata)
             })
