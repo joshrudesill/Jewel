77c33c3f0d224df2797ff8f3956fcd563158e145
Joshua Rudesill <joshrudesill@gmail.com>
2022-10-19 12:49:34 +0200

    title

--- pages/_app.js
+++ pages/_app.js
@@ -2,19 +2,25 @@ import '../styles/mycolors.scss'
 import '../styles/globals.css'
 import 'bulma-tooltip'
 import Script from 'next/script'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return <>
+  <Head>
+    <title>
+      Jewel
+    </title>
+  </Head>
   <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=G-8X7CQ21G90`} />
 
-<Script strategy="afterInteractive" id='gtag'>
-    {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-8X7CQ21G90');
-    `}
-</Script>
+  <Script strategy="afterInteractive" id='gtag'>
+      {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-8X7CQ21G90');
+      `}
+  </Script>
   <Component {...pageProps} />
   <Script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></Script>
   <Script type="module" src="https://www.jsdelivr.com/package/npm/@creativebulma/bulma-tooltip"></Script>
